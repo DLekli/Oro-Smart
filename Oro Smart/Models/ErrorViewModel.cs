8170513be209c5226468f8a059109d20f7bb484a
Denis <Denis@DESKTOP-2179KCJ>
2023-12-05 14:04:20 +0100

    Add project files.

---
+++ Oro Smart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Oro_Smart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
